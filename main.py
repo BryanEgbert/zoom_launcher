92b87a95bf17501e269c91845e7a58b4de39c323
Bryan <bryanegbert561@gmail.com>
2020-10-12 18:00:41 +0700

    Add treeview

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ root.config(menu=menu)
 tree = ttk.Treeview(root)
 
 # Open Input windows
+
+
 def open_input_window():
-    
+
     # Initialize windows
     input_window = Toplevel()
     input_window.title("enter link")
@@ -26,20 +28,20 @@ def open_input_window():
     name_input.grid(row=0, column=1)
     link_input = Entry(input_window)
     link_input.grid(row=1, column=1)
-    
+
     # Temporary function
     def print_input():
         get_name = name_input.get()
         get_link = link_input.get()
         print(get_name, get_link)
-    
+
     # Buttons to start the function
     button = Button(input_window, text="Add", command=print_input)
     button.grid(row=2, column=1)
 
 
 # Initialize menu
-filemenu=Menu(menu)
+filemenu = Menu(menu)
 menu.add_cascade(label="File", menu=filemenu)
 filemenu.add_command(label="New", command=open_input_window)
 
@@ -60,16 +62,17 @@ tree.heading("auto-column", text="auto", anchor=W)
 
 # Temporary data
 data = [
-    ["Chemistry", "12:45", "Yes" ],
+    ["Chemistry", "12:45", "Yes"],
     ["Math", "1:45", "Yes"],
     ["History", "10:00", "Yes"]
 ]
-count=0
+count = 0
 
-# Looping over the data and 
+# Looping over the data and
 # add rows based on the data
 for data in data:
-    tree.insert(parent='', index='end', iid=count, text='', values=(data[0], data[1], data[2]))
+    tree.insert(parent='', index='end', iid=count, text='',
+                values=(data[0], data[1], data[2]))
     count += 1
 
 # Putting tree column to windows
