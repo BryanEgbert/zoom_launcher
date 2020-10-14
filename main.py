31c2c44e87a90e6ad018fbb27ec4dd4e371d0ac6
Bryan <bryanegbert561@gmail.com>
2020-10-14 12:43:08 +0700

    Add method to add by ID

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ root.config(menu=menu)
 tree = ttk.Treeview(root)
 
 # Open Input windows
-def open_input_window():
+def open_input_link_window():
 
     # Initialize windows
     input_window = Toplevel()
@@ -74,6 +74,7 @@ def open_input_window():
         get_link = link_input.get()
         get_time = time_input.get()
         get_check = checkbox_var.get()
+        get_method = "Link"
 
         # Input validation
         # If entry field is empty, show warning box
@@ -85,10 +86,10 @@ def open_input_window():
                 convert_get_time = datetime.datetime.strptime(get_time, '%H:%M').time()
 
                 tree.insert(parent='', index='end', iid=count, text='',
-                            values=(get_day, get_name, get_time, get_check))
+                            values=(get_day, get_name, get_time, get_check, get_method))
 
                 with open('save.txt', 'a') as file:
-                    file.write(get_day+","+get_name+","+get_time+","+get_check+","+get_link+"\n")
+                    file.write(get_day+","+get_name+","+get_time+","+get_check+","+get_method+","+get_link+"\n")
                     count += 1
 
                 input_window.destroy()
@@ -100,14 +101,109 @@ def open_input_window():
     button = Button(input_window, text="Add", command=get_input)
     button.grid(row=5, column=1)
 
+def open_input_id_window():
+
+    # Initialize windows
+    input_window = Toplevel()
+    input_window.title("enter link")
+    input_window.geometry("200x200")
+
+
+    # Label
+    day_label = Label(input_window, text="Day")
+    name_label = Label(input_window, text="Name")
+    id_label = Label(input_window, text="Meeting ID")
+    pass_label = Label(input_window, text="Password")
+    time_label = Label(input_window, text="Time")
+
+    # Put label into screen
+    day_label.grid(row=0, column=0)
+    name_label.grid(row=1, column=0)
+    id_label.grid(row=2, column=0)
+    pass_label.grid(row=3, column=0)
+    time_label.grid(row=4, column=0)
+
+    days = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+    ]
+
+    checkbox_var = StringVar()
+    dropdown_var = StringVar()
+    dropdown_var.set("Monday")
+
+    # Entry field
+    day_input = OptionMenu(input_window, dropdown_var, *days)
+    name_input = Entry(input_window)
+    id_input = Entry(input_window)
+    pass_input = Entry(input_window)
+    time_input = Entry(input_window)
+    checkbox = Checkbutton(input_window, text="auto launch",
+                           variable=checkbox_var, onvalue="Yes", offvalue="No")
+
+    # Deselect checkbox
+    checkbox.deselect()
+
+    # Put entry field to screen
+    day_input.grid(row=0, column=1)
+    name_input.grid(row=1, column=1)
+    id_input.grid(row=2, column=1)
+    pass_input.grid(row=3, column=1)
+    time_input.grid(row=4, column=1)
+    checkbox.grid(row=5, column=1)
+
+    # Add user input to treeview
+    def get_input():
+        global count
+        global data
+
+        get_day = dropdown_var.get()
+        get_name = name_input.get()
+        get_id = id_input.get()
+        get_pass = pass_input.get()
+        get_time = time_input.get()
+        get_check = checkbox_var.get()
+        get_method = "Meeting ID"
+
+        # Input validation
+        # If entry field is empty, show warning box
+        if (get_name == "" or get_id == "" or get_pass == "" or get_time == ""):
+            messagebox.showwarning("Warning", "Fields must not be empty!")
+        # Check if time field value is 24 hour format
+        elif (get_time != "" and get_day != ""):
+            try:
+                convert_get_time = datetime.datetime.strptime(get_time, '%H:%M').time()
+
+                tree.insert(parent='', index='end', iid=count, text='',
+                            values=(get_day, get_name, get_time, get_check, get_method))
+
+                with open('save.txt', 'a') as file:
+                    file.write(get_day+","+get_name+","+get_time+","+get_check+","+get_method+","+get_id+","+get_pass+"\n")
+                    count += 1
+
+                input_window.destroy()
+            except ValueError:
+                messagebox.showwarning("Warning", "Time field not valid!")
+ 
+
+    # Buttons to start the function
+    button = Button(input_window, text="Add", command=get_input)
+    button.grid(row=6, column=1)
+
 
 # Initialize menu
 filemenu = Menu(menu)
 menu.add_cascade(label="File", menu=filemenu)
-filemenu.add_command(label="New", command=open_input_window)
+filemenu.add_command(label="Add by link", command=open_input_link_window)
+filemenu.add_command(label="Add by ID", command=open_input_id_window)
 
 # Create tree column
-tree["columns"] = ("day-column","name-column", "time-column", "auto-column")
+tree["columns"] = ("day-column","name-column", "time-column", "auto-column", "method-column")
 
 # Formatting the tree column
 tree.column("#0", width=0, stretch=NO)
@@ -115,6 +211,7 @@ tree.column("day-column", width=70, minwidth=30)
 tree.column("name-column", width=70, minwidth=30)
 tree.column("time-column", width=50, minwidth=30)
 tree.column("auto-column", width=50, minwidth=30)
+tree.column("method-column", width=70, minwidth=30)
 
 # Add heading to each column
 tree.heading("#0", text="", anchor=W)
@@ -122,6 +219,7 @@ tree.heading("day-column", text="day", anchor=W)
 tree.heading("name-column", text="name", anchor=W)
 tree.heading("time-column", text="time", anchor=W)
 tree.heading("auto-column", text="auto", anchor=W)
+tree.heading("method-column", text="method", anchor=W)
 
 data = []
 count = len(data)
@@ -136,10 +234,19 @@ with open('save.txt', 'r') as file:
     # Check the content of each list inside a data list
     # and insert it to treeview
     for record in data:
+        global data
+        global count
+        # If there is a content inside the record list
+        # then insert the list to treeview 
         if(len(record) > 1):
             tree.insert(parent='', index='end', iid=count, text='',
-                        values=(record[0], record[1], record[2], record[3]))
+                        values=(record[0], record[1], record[2], record[3], record[4]))
             count += 1
+            if(record[3] == "Yes" and record[4] == "Link"):
+                
+                
+        # Else if the content of the record is empty or ['']
+        # Delete the empty record
         elif(len(record) < 1):
             tree.delete(record)
 
