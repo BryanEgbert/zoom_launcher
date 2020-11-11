17c8c3017bcb33397e897fd7ac750365b6254a42
Bryan <bryanegbert561@gmail.com>
2020-11-11 12:54:50 +0700

    remove logging file handler

--- zoom_launcher.py
+++ zoom_launcher.py
@@ -22,8 +22,7 @@ root.geometry("500x400")
 root.config(menu=menu)
 
 # Logging config
-logging.basicConfig(level=logging.INFO, filename='image_location.log',
-                    format='%(asctime)s:%(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(message)s')
 
 # Treeview
 tree = ttk.Treeview(root, height=36)
